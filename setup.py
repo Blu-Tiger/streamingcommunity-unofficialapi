d5b1e470c51dd25b3414ce3c42a71b4c6ffe10c4
BluTiger <65156144+Blu-Tiger@users.noreply.github.com>
2024-10-11 01:11:00 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="streamingcommunity-unofficialapi",
-    version="1.0.1",
+    version="1.0.2",
     author="BluTiger",
     description="A simple unofficial api for the italian StreamingCommunity website.",
     long_description=long_description,
